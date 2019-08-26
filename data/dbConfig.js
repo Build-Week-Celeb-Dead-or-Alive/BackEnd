4a23364c07716a40ae08d5bb012c3e3db6f5b604
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-26 10:42:11 -0700

    changes for deployment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
 const dbEngine = process.env.DB || 'development'
-const knexConfig = require('../../knexfile.js')[dbEngine];
+const knexConfig = require('../knexfile.js')[dbEngine];
 
 module.exports = require('knex')(knexConfig);
