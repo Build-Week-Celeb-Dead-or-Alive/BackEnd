13bfb038b4485b4ab64fdcd1d301f76a8355fc60
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-27 11:12:15 -0700

    updating seeds with images for testing

--- data/seeds/001-celebs.js
+++ data/seeds/001-celebs.js
@@ -1,10 +1,12 @@
 exports.seed = function(knex) {
   return knex('celebs').insert([{
     "name": "Laura Daugherty",
+    "celebImage" : "images/Billygoathill.jpeg",
     "isDead": false,
     "dob": "1990-09-25",
   },{
     "name": "Natalie Daugherty",
+    "celebImage" : "images/Billygoathill.jpeg",
     "isDead": false,
     "dob": "1990-03-30",
   }])
